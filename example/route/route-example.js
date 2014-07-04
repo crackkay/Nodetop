24774f934822e3ba489b75113e7a1f486360cd0c
crackkay <crackkay@gmail.com>
2014-07-04 11:34:44 +0800

    add route regexp get/post

---
+++ example/route/route-example.js
@@ -0,0 +1,14 @@
+var App = require("../..").App,
+    app = new App;
+
+    app.get("/about",function(req,res){
+        res.write("my name is leo");
+        res.end();
+    })
+
+    app.get("/contact/*/:id/ok",function(req,res){
+        res.write("contact me use QQ 1405491181");
+        res.end();
+    })
+
+    app.listen(3000);
